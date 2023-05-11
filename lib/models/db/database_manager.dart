feb9b890760f46c54896339ecc11eeed9b484779
aquaman1182 <hal.bandotakumi@gmail.com>
2023-05-12 05:47:18 +0900

    fix

--- lib/models/db/database_manager.dart
+++ lib/models/db/database_manager.dart
@@ -147,35 +147,6 @@ class DatabaseManager {
       yield todo;
     }
   }
-//   Stream<List<Todo>> fetchTodoListStream() async* {
-//     final user = FirebaseAuth.instance.currentUser;
-//     if (user == null) yield [];
-
-//     while (true) {
-//       final snapshot = await _db
-//           .collection('todo')
-//           .where('userId', isEqualTo: user!.uid)
-//           .get();
-
-//       final List<Todo> todo = [];
-
-//       for (final doc in snapshot.docs) {
-//         final id = doc.id;
-//         final data = doc.data();
-//         final task = data['task'];
-//         final userId = data['userId'];
-
-//         final userSnapshot = await _db.collection('users').doc(userId).get();
-//         final userData = userSnapshot.data();
-//         final name = userData?['name'] ?? '';
-
-//         todo.add(Todo(id: id, task: task, name: name, userId: userId));
-//       }
-
-//     yield todo;
-//   }
-// }
-
 
   //修正済　動作確認済
   Future<void> delete(Todo todo) async {
